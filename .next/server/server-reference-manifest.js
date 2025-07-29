b33febf98f08bc2d170d7676a3631dde88988147
Wilson Oyem <85078140+oyemwilson@users.noreply.github.com>
2025-07-29 12:28:44 +0100

    npm build

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"BaIF4c4095DnXira3jDOV4rtXn/E64/zhytR83JBFbY=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
--- .next/static/chunks/src_app_page_7966b1cc.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_72282523._.js",
-  "static/chunks/src_app_pages_Topnav_jsx_a27c386b._.js"
-],
-    source: "dynamic"
-});
